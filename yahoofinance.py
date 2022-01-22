fdefe9f229b0d41f34947526dca8441f920acd07
mahout83 <nareshpolisetti@gmail.com>
2022-01-21 18:48:29 -0600

    Update yahoofinance.py

--- yahoofinance.py
+++ yahoofinance.py
@@ -1,15 +1,16 @@
 import requests
 import streamlit as st
 
-url = "https://yfapi.net/v6/finance/quote"
+#url = "https://yfapi.net/v6/finance/quote"
+url = "https://yfapi.net/v6/finance/quote/marketSummary?lang=en&region=US"
 
-querystring = {"symbols":"AAPL,BTC-USD,EURUSD=X"}
+#querystring = {"symbols":"AAPL,BTC-USD,EURUSD=X"}
 
 headers = {
     'x-api-key': "BZiD57EbwJ1dYlFDmRlZf3J1pM4GWiwT6KjXy1YQ"
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+response = requests.request("GET", url, headers=headers)
 
 print(response.text)
 st.write(response.text)
