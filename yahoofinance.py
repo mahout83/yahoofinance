3e2f885411df05fe8bce9158eed9636ccac6116a
mahout83 <nareshpolisetti@gmail.com>
2022-01-21 19:02:09 -0600

    Update yahoofinance.py

--- yahoofinance.py
+++ yahoofinance.py
@@ -7,12 +7,11 @@ url = "https://yfapi.net/v6/finance/quote/marketSummary?lang=en&region=US"
 #querystring = {"symbols":"AAPL,BTC-USD,EURUSD=X"}
 
 headers = {
-    'x-api-key': "BZiD57EbwJ1dYlFDmRlZf3J1pM4GWiwT6KjXy1YQ"
+    'x-api-key': "BZiD57EbwJ1dYlFDmRlZf3J1pM4GWiwT6KjXy1YQ" \
+     'accept: application/json' 
     }
 
 response = requests.request("GET", url, headers=headers)
 
 print(response.text)
-#st.write(response.text)
-q = type(response.text)
-st.write(type(q))
+st.write(response.text)
