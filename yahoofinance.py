46252a9c7fcb7d4ec572dee731357330d3a7d466
mahout83 <nareshpolisetti@gmail.com>
2022-01-21 19:36:34 -0600

    Update yahoofinance.py

--- yahoofinance.py
+++ yahoofinance.py
@@ -1,7 +1,7 @@
 import requests
 import streamlit as st
 import yfinance as yf
-z = dir(yf)
+#z = dir(yf)
 
 #url = "https://yfapi.net/v6/finance/quote"
 #url = "https://yfapi.net/v6/finance/quote/marketSummary?lang=en&region=US"
@@ -16,4 +16,6 @@ z = dir(yf)
 #response = requests.request("GET", url, headers=headers)
 
 #print(response.text)
-st.write(z)
+#st.write(z)
+msft = yf.Ticker("MSFT")
+st.write(msft.info)
